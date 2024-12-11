fcfdc9ff2c854dbe753625eaefa88fa26fa7ece1
AlessioCugliari <63005089+AlessioCugliari@users.noreply.github.com>
2024-12-12 00:50:02 +0100

    Initial commit

---
+++ scalartes-be/src/main/java/it/ex/scalartes_be/ScalartesBeApplication.java
@@ -0,0 +1,13 @@
+package it.ex.scalartes_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalartesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalartesBeApplication.class, args);
+	}
+
+}
---
+++ scalartes-be/src/test/java/it/ex/scalartes_be/ScalartesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ex.scalartes_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalartesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
