7a073c7836bf8c3040fc54cb2b53431fcd472790
DESKTOP-HSD8GTE\Leonardo <leonardo.grillo.1995@gmail.com>
2025-01-14 19:13:35 +0100

    feat: aggiunte classi per la torre

---
+++ scalartes-be/src/main/java/it/ex/scalartes_be/entity/Torre.java
@@ -0,0 +1,22 @@
+package it.ex.scalartes_be.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "TORRE")
+public class Torre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @OneToMany
+    private List<Linea> linee;
+
+}
---
+++ scalartes-be/src/main/java/it/ex/scalartes_be/entity/Via.java
@@ -0,0 +1,27 @@
+package it.ex.scalartes_be.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "VIA")
+public class Via {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String colore;
+    //private grado; todo enums
+    private Utente tracciatore;
+    private List<String> note;
+    //private foto foto; todo
+    private LocalDate dataTracciamento;
+}
