91c3250bead0ec25ab3b3818435728c3aef68911
AlessioCugliari <cugliari.alessio@gmail.com>
2025-01-30 19:09:40 +0100

    feat: Aggiunti muroRepository e endpoint getById and DeleteById

--- scalartes-be/src/main/java/it/ex/scalartes_be/controller/MuroController.java
+++ scalartes-be/src/main/java/it/ex/scalartes_be/controller/MuroController.java
@@ -3,22 +3,28 @@ package it.ex.scalartes_be.controller;
 import it.ex.scalartes_be.entity.Muro;
 import it.ex.scalartes_be.service.MuroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/muro")
 public class MuroController {
 
-    @Autowired
     private MuroService muroService;
 
-    /*@GetMapping("/{id}")
+    @Autowired
+    public MuroController(MuroService muroService) {
+        this.muroService = muroService;
+    }
+
+    @GetMapping("/{id}")
     public Muro getMuroById(@RequestParam Long id){
         return muroService.getMuroById(id);
-    }*/
+    }
+
+    @DeleteMapping("/delete/")
+    public void deleteMuroById(@RequestParam("muroId") Long id){
+        muroService.deleteMuroById(id);
+    }
 
 
 
--- scalartes-be/src/main/java/it/ex/scalartes_be/service/MuroService.java
+++ scalartes-be/src/main/java/it/ex/scalartes_be/service/MuroService.java
@@ -1,7 +1,26 @@
 package it.ex.scalartes_be.service;
 
+import it.ex.scalartes_be.entity.Muro;
+import it.ex.scalartes_be.repository.MuroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MuroService {
+
+    private MuroRepository muroRepository;
+
+    @Autowired
+    public MuroService(MuroRepository muroRepository) {
+        this.muroRepository = muroRepository;
+    }
+
+
+    public Muro getMuroById(Long id) {
+        return muroRepository.getById(id);
+    }
+
+    public void deleteMuroById(Long id) {
+        muroRepository.deleteById(id);
+    }
 }
