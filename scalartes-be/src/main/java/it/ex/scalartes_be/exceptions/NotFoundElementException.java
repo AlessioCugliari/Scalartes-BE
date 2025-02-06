343cdc08a179ebf49f36e2946cc93f5835284af2
AlessioCugliari <cugliari.alessio@gmail.com>
2025-02-06 19:08:48 +0100

    feat: aggiunta NotFoundElementException e ExceptionHandeler

---
+++ scalartes-be/src/main/java/it/ex/scalartes_be/exceptions/NotFoundElementException.java
@@ -0,0 +1,7 @@
+package it.ex.scalartes_be.exceptions;
+
+public class NotFoundElementException extends Exception{
+    public NotFoundElementException(String message) {
+        super(message);
+    }
+}
---
+++ scalartes-be/src/main/java/it/ex/scalartes_be/exceptions/RestResponseEntityExceptionHandeler.java
@@ -0,0 +1,21 @@
+package it.ex.scalartes_be.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestResponseEntityExceptionHandeler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = NotFoundElementException.class)
+    protected ResponseEntity<Object> handleNotFoundElementException(Exception e, WebRequest request){
+
+        String bodyOfResponse = NotFoundElementException.class.getName() + " " + e.getMessage();
+        return handleExceptionInternal(e,bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT,request);
+    }
+}
