72461c000b64ff3c2962195a6ff59d2a4aa0c339
DESKTOP-HSD8GTE\Leonardo <leonardo.grillo.1995@gmail.com>
2025-02-27 18:32:43 +0100

    WIP: aggiunti metodi su parete controller e parametri su parete dto

--- scalartes-be/src/main/java/it/ex/scalartes_be/controller/PareteController.java
+++ scalartes-be/src/main/java/it/ex/scalartes_be/controller/PareteController.java
@@ -1,9 +1,28 @@
 package it.ex.scalartes_be.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import it.ex.scalartes_be.entity.Parete;
+import it.ex.scalartes_be.service.PareteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/linea")
+@RequestMapping("/parete")
 public class PareteController {
+
+    private PareteService pareteService;
+
+    @Autowired
+    public PareteController(PareteService pareteService) {
+        this.pareteService = pareteService;
+    }
+
+    @GetMapping("/{id}")
+    public Parete getPareteById(@PathVariable Long id) {
+        return pareteService.getPareteById(id);
+    }
+
+    @DeleteMapping("/delete/")
+    public void deletePareteById(@RequestParam("pareteid") Long id) {
+        pareteService.deletePareteById(id);
+    }
 }
--- scalartes-be/src/main/java/it/ex/scalartes_be/dto/PareteDTO.java
+++ scalartes-be/src/main/java/it/ex/scalartes_be/dto/PareteDTO.java
@@ -1,5 +1,13 @@
 package it.ex.scalartes_be.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class PareteDTO {
 
+    private List<String> listVie;
 }
