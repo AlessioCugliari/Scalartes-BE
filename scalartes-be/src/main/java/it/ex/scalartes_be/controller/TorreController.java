e152cd8f885e38f6c7e88394d40486d52449f439
DESKTOP-HSD8GTE\Leonardo <leonardo.grillo.1995@gmail.com>
2025-03-13 18:42:46 +0100

    feat: aggiunti TorreController e TorreService

--- scalartes-be/src/main/java/it/ex/scalartes_be/controller/TorreController.java
+++ scalartes-be/src/main/java/it/ex/scalartes_be/controller/TorreController.java
@@ -1,10 +1,12 @@
 package it.ex.scalartes_be.controller;
 
+import it.ex.scalartes_be.entity.Torre;
 import it.ex.scalartes_be.entity.Via;
+import it.ex.scalartes_be.exceptions.NotFoundElementException;
 import it.ex.scalartes_be.service.TorreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/torre")
@@ -12,4 +14,25 @@ public class TorreController {
 
     @Autowired
     private TorreService torreService;
+
+    @Autowired
+    public TorreController(TorreService torreService) {
+        this.torreService = torreService;
+    }
+
+    @GetMapping("/{id}")
+    public Torre getTorreById(@PathVariable Long id) throws NotFoundElementException {
+        return torreService.getTorreById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Torre addTorre() {
+        return torreService.addTorre();
+    }
+
+    @DeleteMapping("/delete/")
+    public void deleteTorreById(@RequestParam("muroid") Long id) {
+        torreService.deleteTorreById(id);
+    }
 }
