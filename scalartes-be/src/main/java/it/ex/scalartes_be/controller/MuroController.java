6496fc77c7705965d269342ee1a6e81d20abad90
AlessioCugliari <cugliari.alessio@gmail.com>
2025-05-22 18:42:39 +0200

    feat: aggiunti log su Muro Controller e Service. Aggiunta eccezione muro not found in get muro

--- scalartes-be/src/main/java/it/ex/scalartes_be/controller/MuroController.java
+++ scalartes-be/src/main/java/it/ex/scalartes_be/controller/MuroController.java
@@ -1,12 +1,15 @@
 package it.ex.scalartes_be.controller;
 
 import it.ex.scalartes_be.entity.Muro;
+import it.ex.scalartes_be.exceptions.NotFoundElementException;
 import it.ex.scalartes_be.service.MuroService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/muro")
+@Slf4j
 public class MuroController {
 
     private MuroService muroService;
@@ -17,17 +20,20 @@ public class MuroController {
     }
 
     @GetMapping("/{id}")
-    public Muro getMuroById(@PathVariable Long id){
+    public Muro getMuroById(@PathVariable Long id) throws NotFoundElementException {
+        log.info("Request GET /muro/{}", id);
         return muroService.getMuroById(id);
     }
 
     @PostMapping("/add")
     public Muro addMuro(){
+        log.info("Request POST muro/add/ {}");
         return muroService.addMuro();
     }
 
     @DeleteMapping("/delete/")
     public void deleteMuroById(@RequestParam("muroId") Long id){
+        log.info("Request DELETE /muro/{}", id);
         muroService.deleteMuroById(id);
     }
 
